2bf5fd30d1b901c34a050c14f84fb54a3bf15308
Emeamos <Emediongamos.engr@gmail.com>
2023-04-18 21:29:46 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dbConnect from "./config/dbConnect.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import subjectRoute from "./routes/subject.js";
 
 dotenv.config({path:'./.env'})
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 dbConnect();
-
+app.use(cors({credentials: true, origin: 'http://127.0.0.1:5000'}));
 app.use("/api/subject", subjectRoute)
 
 const PORT = process.env.PORT || 5000;
