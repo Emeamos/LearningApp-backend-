1c8b8d62c230094bc02cab5df59c0cc224ee0f04
Emeamos <Emediongamos.engr@gmail.com>
2023-04-26 03:10:39 +0100

    refactoring

--- controller/topic.js
+++ controller/topic.js
@@ -25,43 +25,7 @@ export const addTopic = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-// export const getTopicsBySubjectId = async (req, res) => {
-//   try {
-//     const { subjectId } = req.params;
-//     if (!ObjectId.isValid(subjectId)) {
-//       return res.status(404).json({ error: "Invalid subjectId" });
-//     }
-//     const subject = await Subject.findById(subjectId);
-//     if (!subject) {
-//       return res.status(404).json({ error: "Subject not found" });
-//     }
-//     const topics = await Topic.find({ subject: subjectId }).populate("subject");
-//     res.status(200).json(topics);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
 
-// export const getTopicsBySubjectId = async (req, res) => {
-//   try {
-//     const { subjectId } = req.params;
-//     const isValidObjectId = mongoose.Types.ObjectId.isValid(subjectId);
-// if (!isValidObjectId) {
-//   return res.status(404).json({ error: "Invalid subjectId" });
-// }
-//     const subject = await Subject.findById(subjectId).populate('topics');
-//     if (!subject) {
-//       return res.status(404).json({ error: "Subject not found" });
-//     }
-//     //const topics = await Topic.find({ subject: subjectId }).populate("subject");//
-//     const topics = await Subject.find(subjectId).populate("topics")
-//     res.status(200).json(topics);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
 export const getTopicsBySubjectId = async (req, res) => {
   try {
     const { subjectId } = req.params;
@@ -79,39 +43,6 @@ export const getTopicsBySubjectId = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
-
-
-
-// export const getTopicById = async (req, res) => {
-//   try {
-//     const { subjectId, topicId } = req.params;
-//     const topic = await Topic.findOne({ _id: topicId, subject: subjectId });
-//     if (!topic) {
-//       return res.status(404).json({ error: "Topic not found" });
-//     }
-//     res.status(200).json(topic);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
-// export const getTopicById = async (req, res) => {
-//   try {
-//     const { subjectId, topicId } = req.params;
-//     const subject = await Subject.findById(subjectId).populate('topics');
-//     if (!subject) {
-//       return res.status(404).json({ error: "Subject not found" });
-//     }
-//     const topic = subject.topics.find(t => t._id.toString() === topicId);
-//     if (!topic) {
-//       return res.status(404).json({ message: 'topic not found for this course' });
-//     }
-//     res.status(200).json(topic);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
 
 export const getTopicById = async (req, res) => {
   try {
